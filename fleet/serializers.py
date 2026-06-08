2c272079c157715d0a93aadf2446dae9c3b54b75
varuncareergize <varun.124150@gmail.com>
2026-06-08 22:26:33 +0530

    new_Cahges

--- fleet/serializers.py
+++ fleet/serializers.py
@@ -3,6 +3,8 @@ from .models import Vehicle
 
 
 class VehicleSerializer(serializers.ModelSerializer):
+    monthly_mileage = serializers.SerializerMethodField()
+
     class Meta:
         model = Vehicle
         fields = [
@@ -11,7 +13,9 @@ class VehicleSerializer(serializers.ModelSerializer):
             'vehicle_code',
             'plate_number',
             'vehicle_type',
-            'current_mileage',
+            'monthly_start_mileage',
+            'monthly_end_mileage',
+            'monthly_mileage',
             'fuel_level',
             'oil_level',
             'battery_health',
@@ -19,4 +23,7 @@ class VehicleSerializer(serializers.ModelSerializer):
             'next_service_date',
             'status',
         ]
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'monthly_mileage']
+
+    def get_monthly_mileage(self, obj):
+        return obj.monthly_end_mileage - obj.monthly_start_mileage
